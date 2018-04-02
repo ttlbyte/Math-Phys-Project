d8eaed4a9b7c5605b03e18ac2dbb850a72750830
Shiqi Ruan <tuh14623@cst11220.tu.temple.edu>
2018-04-02 12:52:10 -0400

    first commit

---
+++ PA3/prog1.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int
+main()
+{
+	double *p, *l, *d, *u, *r, x, dx, xL, xR, pL, pR, phi, L;
+	double exact(double, double, double);
+	int n, i;
+	int td_solve(double*, double*, double*, double*, double*, int);
+	FILE *fp;
+
+	printf("Enter phi, L, pL, pR: ");
+	scanf("%le %le %le %le",&phi, &L, &pL, &pR);
+
+    xL=-L/2;
+    xR=L/2;
+	printf("Enter N: ");
+	scanf("%d", &n);
+
+	p = (double*)malloc(n*sizeof(double));
+	l = (double*)malloc(n*sizeof(double));
+	d = (double*)malloc(n*sizeof(double));
+	u = (double*)malloc(n*sizeof(double));
+	r = (double*)malloc(n*sizeof(double));
+
+	dx = L/(n-1);
+
+	for (i = 0; i <= n-1; i++) {
+
+		x =-L/2 + i*dx;
+
+		r[i] = exact(x, phi, L);
+		d[i] = -2.0;
+		l[i] = u[i] = 1.0;
+	}
+    u[0] = 2;
+    l[n-1]=2;
+	r[1] -= phi*(1-exact(-L/2, phi, L)) ;
+	r[n-2] -= -phi*(1-exact(L/2, phi, L));
+
+	p[0] = pL;
+	p[n-1] = pR;
+
+	if (td_solve(l, d, u, r, p, n) != 0) {
+		fprintf(stderr, "td_solve() failed!\n");
+		exit(2);
+	}
+
+	fp = fopen("2pt.out", "w");
+
+	for (i = 0; i < n; i++) {
+
+		x = -L/2 + i*dx;
+
+		fprintf(fp, "%le %le %le\n", x, p[i], exact(x, phi, L));
+	}
+
+	fclose(fp);
+	exit(0);
+}
+
+double
+exact(double x, double phi, double L)
+{
+	return phi*cosh(x)/(phi*cosh(L/2)+sinh(L/2));
+}
+
+
+int
+td_solve(double *l, double *d, double *u, double *r, double *y, int n)
+{
+    int i;
+
+/*
+ * do Gaussian elimination:
+ */
+    for (i = 1; i < n; i++) {
+
+        if (d[i-1] == 0.0) return 1;
+
+        d[i] -= l[i]*u[i-1]/d[i-1];
+        r[i] -= l[i]*r[i-1]/d[i-1];
+
+    }
+
+/*
+ * do back-substitution:
+ */
+    if (d[n-1] == 0.0) return 2;
+
+    y[n-1] = r[n-1]/d[n-1];
+
+    for (i = n-2; i >= 0; i--) {
+
+        if(d[i] == 0.0) return 2;
+
+        y[i] = (r[i] - u[i]*y[i+1])/d[i];
+    }
+
+    return 0;
+}
